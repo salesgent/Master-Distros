e8f2210a467859ac2281e0ade3ad215e054370e8
jayzala33 <jayzala33@gmail.com>
2023-08-16 14:32:30 +0530

    changes

--- pages/account/register.jsx
+++ pages/account/register.jsx
@@ -438,8 +438,7 @@ const RegisterPage = () => {
                     render={({ field }) => <input type="checkbox" {...field} required />}
                   />
                   <Typography>
-                    I have read and agree to 101 Distributors Fl <span>Terms of Use </span> &{" "}
-                    <span>Privacy Policy.</span>
+                    I have read and agree to Master Distro <span>Terms of Use </span> & <span>Privacy Policy.</span>
                   </Typography>
                 </div>
               </Grid>
