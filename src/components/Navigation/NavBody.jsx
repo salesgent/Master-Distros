cce76aed12e0f1d59badf69d1e0e5e7e9266f6a8
jayzala33 <jayzala33@gmail.com>
2023-09-06 14:52:11 +0530

    fix menu

--- src/components/Navigation/NavBody.jsx
+++ src/components/Navigation/NavBody.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useRef, useLayoutEffect } from "react";
 import Link from "next/link";
+import React, { useLayoutEffect, useRef, useState } from "react";
 ////////////////////////////////////////////////////////////////
-import styles from "./style.module.scss";
 import styled from "styled-components";
+import styles from "./style.module.scss";
+import { Grid } from "@mui/material";
 
 const Overlay = styled.div``;
 
@@ -13,15 +14,15 @@ const NavBody = ({ data, id, setshow }) => {
   const menuBody = useRef(null);
 
   useLayoutEffect(() => {
-    const filter = list?.find((v) => v?.subCategories.length > 0);
-    // console.log(filter, "item");
-    if (Boolean(filter)) {
-      setHorMenu(true);
-    } else {
-      setHorMenu(false);
-    }
+    list?.forEach((sub) => {
+      if (sub?.subCategories) {
+        setHorMenu(true);
+      } else {
+        setHorMenu(false);
+      }
+    });
   }, []);
-  // console.log(list, "list");
+
   if (horMenu) {
     return (
       // <Overlay
@@ -30,48 +31,60 @@ const NavBody = ({ data, id, setshow }) => {
       //   lengthSubCat={list.length}
       //   pId={`overlay-${id}`}
       //   id={`id-overlay-${id}`}
-      //   style={{ width: "100%" }}
       // >
       <div className={styles.horzMenu}>
-        {list?.map((cat, i) => (
-          <div key={i} className={styles.col}>
-            <Link
-              href={{
-                pathname: `/products/${cat.alias}/${cat.id}`,
-              }}
-            >
-              <a
-                className={styles.headerLink}
-                onClick={() => {
-                  // dispatch(setActiveProductId(cat?.id));
-
-                  setshow(-1);
-                }}
+        <Grid container>
+          {list?.map((cat, i) => {
+            return (
+              <Grid
+                container
+                item
+                key={i}
+                lg={list?.length > 6 ? 2 : true}
+                md={list?.length > 4 ? 3 : true}
+                sm={list?.length > 3 ? 4 : true}
               >
-                {cat.name}{" "}
-              </a>
-            </Link>
-            <div className={styles.subCat}>
-              {cat?.subCategories?.map((sub, i) => (
-                <Link
-                  href={{
-                    pathname: `/products/${sub.alias}/${sub.id}`,
-                  }}
-                  key={i}
-                >
-                  <a
-                    onClick={() => {
-                      // dispatch(setActiveProductId(sub?.id));
-                      setshow(-1);
+                <div key={i} className={styles.col}>
+                  <Link
+                    href={{
+                      pathname: `/products/${cat.alias}/${cat.id}`,
                     }}
                   >
-                    {sub.name}
-                  </a>
-                </Link>
-              ))}
-            </div>
-          </div>
-        ))}
+                    <a
+                      className={styles.headerLink}
+                      onClick={() => {
+                        // dispatch(setActiveProductId(cat?.id));
+
+                        setshow(-1);
+                      }}
+                    >
+                      {cat.name}{" "}
+                    </a>
+                  </Link>
+                  <div className={styles.subCat}>
+                    {cat?.subCategories?.map((sub, i) => (
+                      <Link
+                        href={{
+                          pathname: `/products/${sub.alias}/${sub.id}`,
+                        }}
+                        key={i}
+                      >
+                        <a
+                          onClick={() => {
+                            // dispatch(setActiveProductId(sub?.id));
+                            setshow(-1);
+                          }}
+                        >
+                          {sub.name}
+                        </a>
+                      </Link>
+                    ))}
+                  </div>
+                </div>
+              </Grid>
+            );
+          })}
+        </Grid>
       </div>
       // </Overlay>
     );
